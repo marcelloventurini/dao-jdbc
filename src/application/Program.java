46d38ea55cd922282a3e22e50bbc73a44ab53e2d
Marcello Venturini <marcellofventurini@gmail.com>
2022-11-28 16:41:01 -0300

    Create SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
 	public static void main(String[] args) {
 		Department department = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+		SellerDao sellerDao = DaoFactory.crateSellerDao();
 
 		System.out.println(seller);
 	}
